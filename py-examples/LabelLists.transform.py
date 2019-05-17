5f1c7286316136d9aa2bd44c5ccf5cafd7a64460
EmbraceLife <1227561934@qq.com>
2019-05-17 20:59:15 +0800

    Image.show.py, LabelLists.transform.py

---
+++ py-examples/LabelLists.transform.py
@@ -0,0 +1,14 @@
+from fastai.vision import *
+import fastai.vision as fv
+path_data = untar_data(URLs.MNIST_TINY)
+path_data.ls()
+il = ImageList.from_folder(path_data, convert_mode='L')
+sd = il.split_by_folder('train', 'valid')
+ll = sd.label_from_folder()
+x, y = ll.train[0]
+x.show(y=y)
+x.show(title='test')
+x.show(y=y, title='test')
+print(y,x.shape)
+tfms = ([*rand_pad(padding=3, size=28, mode='zeros')], [])
+ll = ll.transform(tfms)
